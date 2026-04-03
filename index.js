e6b2e7ca625207e469a578971ca730a0bf51e754
aysegulgurcan617-lgtm <aysegulgurcan617@gmail.com>
2026-04-03 17:01:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import http from "http";
 
 const server = http.createServer((req, res) => {
-  res.writeHead(302, {
-    Location: "https://seninsiten.com"
-  });
-  res.end();
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("Site hazır");
 });
 
 server.listen(3000);
