63d0b96f56a4d8da867a9f08b72a094f731eeb77
aysegulgurcan617-lgtm <aysegulgurcan617@gmail.com>
2026-04-03 17:04:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import http from "http";
 
+const PORT = process.env.PORT || 3000;
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
+  res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Site hazır");
 });
 
-server.listen(3000);
+server.listen(PORT, () => {
+  console.log("Server çalışıyor: " + PORT);
+});
